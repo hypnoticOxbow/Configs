f2eb5e60759c058cfb38ff0d9ad61e39553f2b74
Ian Kariniemi <i.kariniemi@gmail.com>
2020-04-06 21:31:45 -0500

    Config Backup 2020-04-06-21-31

--- config-back-up.sh
+++ config-back-up.sh
@@ -2,6 +2,7 @@ set -euxo pipefail
 cp ~/.xmonad/xmonad.hs ./
 cp ~/.xmobarrc ./
 cp ~/.config/nvim/init.vim ./.vimrc
+cp ~/.config/dunst/dunstrc ./
 cp ~/.bashrc ./
 cp ~/.zshrc ./
 cp -r ~/.doom.d/ ./
