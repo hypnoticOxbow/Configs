1237940a35445b399129cdf7b024dab2b847dbe6
Ian Kariniemi <i.kariniemi@gmail.com>
2021-09-16 10:00:50 -0500

    Config Backup 2021-09-16-10-00

--- config-back-up.sh
+++ config-back-up.sh
@@ -7,12 +7,13 @@ cp ~/.config/nvim/init.vim ./.vimrc
 cp ~/.config/dunst/dunstrc ./
 cp ~/.bashrc ./
 cp ~/.zshrc ./
-cp -r ~/.doom.d/ ./
+# cp -r ~/.doom.d/ ./
 cp ~/.Xresources ./
 cp ~/.gitconfig ./
 cp ~/.config/picom/picom.conf ./
 cp ~/.config/kitty/kitty.conf ./
 cp ~/.tridactylrc ./
+cp ~/.local/bin/butler ./
 git add .
 git commit -m "Config Backup $(date +%F-%H-%M)"
 git push
